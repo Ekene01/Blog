8f907c99fb15ddb4b27737365608ab445267007c
Ekene Ikoro <ekeneikoro@gmail.com>
2022-07-28 15:03:44 +0100

    VSing BlogPost.js

--- index.js
+++ index.js
@@ -2,16 +2,25 @@ const express = require('express')
 const app = new express()
 const path = require('path')
 const ejs = require('ejs')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true })
+const BlogPost = require('./models/BlogPost.js')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.listen(4000, () => {
     console.log('App listening on port 4000')
 })
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'))
-    res.render('index');
+app.get('/', async(req, res) => {
+    const blogposts = await BlogPost.find({})
+    res.render('index', {
+        blogposts
+    });
 })
+
 app.get('/about', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'pages/about.html'))
     res.render('about');
@@ -20,7 +29,27 @@ app.get('/contact', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
     res.render('contact');
 })
-app.get('/post', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
-    res.render('post');
+
+app.get('/posts/search', async (req, res) => {
+    const search = await BlogPost.find(req.params.body)
+    res.render('index', {
+        search
+    });
 })
+
+app.get('/post/:id', async(req, res) => {
+    const blogpost = await BlogPost.findById(req.params.id) 
+    res.render('post', {
+        blogpost
+    })
+})
+app.get('/posts/new', (req,res) => {
+    res.render('create')
+})
+app.post('/posts/store', async(req, res) => {
+    //console.log(req.body)
+    //Model creates new doc with browser data
+    await BlogPost.create(req.body)
+        res.redirect('/')
+    })
+
